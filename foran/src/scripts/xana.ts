a849aa3b137f9c7a70b09eb86fd5920e40a6f17b
stackswithans <stexor12@gmail.com>
2021-05-18 12:40:12 +0100

    Connection working

--- foran/src/scripts/xana.ts
+++ foran/src/scripts/xana.ts
@@ -8,6 +8,18 @@ type onTrackCallback = (
 export const joinCall = (callId: String, fn: onTrackCallback) => {
     const hostname = new URL(host).host;
     const channel = new WebSocket(`ws://${hostname}/ws/call`);
+
+    //Send connect message as soon as channel opens
+    channel.onopen = () => {
+        channel.send(
+            JSON.stringify({
+                type: "join_call",
+                callId: callId,
+            })
+        );
+    };
+
+    //Channel message handler
     channel.onmessage = (event) => {
         console.log(event.data);
     };
--- tilbage/call/consumers.py
+++ tilbage/call/consumers.py
@@ -7,29 +7,33 @@ from .models import Call
 class CallConsumer(WebsocketConsumer):
     def connect(self):
         # Add user data here
+        self.group_name = None
         self.accept()
 
     def disconnect(self):
         # Remove user from group if he was connectrd
-        if getattr(self, "group_name", None):
+        if self.group_name:
             async_to_sync(self.channel_layer.group_discard)(
                 self.group_name, self.channel_name
             )
-        self.accept()
 
     def receive(self, text_data):
         msg = json.loads(text_data)
         ms_type = msg["type"]
-        if ms_type == "join-call":
+        if ms_type == "join_call":
             # Add channel to call group
             self.group_name = msg["callId"]
             async_to_sync(self.channel_layer.group_add)(
                 self.group_name, self.channel_name
             )
-            call = Call.objects.get(callId=self.call_id)
-            # Make the first client to connect the polite peer
+            # Send new_participant message to group
+            async_to_sync(self.channel_layer.group_send)(
+                self.group_name, {"type": "new_participant", "message": "ni"}
+            )
+            call = Call.objects.get(callId=self.group_name)
+            # Consider the first client to connect as the polite peer
             call.participants += 1
-            call.participants.save()
+            call.save()
             response = {"type": "join-accept", "polite": "true"}
             if call.participants == 2:
                 response["polite"] = "false"
