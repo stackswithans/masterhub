6e7e8130381fe3e69b2196ed7100e3f60791b429
stackswithans <stexor12@gmail.com>
2021-05-27 15:15:27 +0100

    Make some progress on sign up layout

--- foran/src/routes/SignUp.svelte
+++ foran/src/routes/SignUp.svelte
@@ -1,16 +1,51 @@
 <script lang="typescript">
-    import LoginInput from "../components/LoginInput.svelte";
+    import NavBar from "../components/NavBar.svelte";
 </script>
 
-<div>
-    <LoginInput description="Hello"/>
+<div class="container">
+    <NavBar theme="light"/>
+    <main class="content">
+        <div class="form-layout">
+            <aside class="image"></aside>
+            <aside class="form"></aside>
+        </div>
+    </main>
 </div>
 
 
 <style>
-    div{
+    .container{
+        position: relative;
+        display: flex;
+        flex-direction: column;
         width: 100%;
         height: 100vh;
-        background-color: white;
+        background-color: #141414;
+    }
+
+    .content{
+        display: flex;
+        flex-grow: 1;
+        justify-content: center;
+        align-items: center;
+    }
+
+
+    .form-layout{
+        display: flex;
+        width: 78%;
+        height: 68%;
+    }
+
+    .form-layout .image{
+        width: 40%;
+        height: 100%;
+        background-color: var(--color-3);
+    }
+
+    .form-layout .form{
+        flex-grow: 1;
+        height: 100%;
+        background-color: var(--color-4);
     }
 </style>
