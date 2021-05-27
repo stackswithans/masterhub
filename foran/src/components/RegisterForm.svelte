8f4bc79965e615b7bb0c5f94108cd40664a085ea
stackswithans <stexor12@gmail.com>
2021-05-27 20:49:10 +0100

    Add register input

--- foran/src/components/RegisterForm.svelte
+++ foran/src/components/RegisterForm.svelte
@@ -1,6 +1,7 @@
 <script lang="typescript">
     import RegisterHeader  from "../components/RegisterHeader.svelte";
     import RegisterInput from "../components/RegisterInput.svelte";
+    import RegisterRadio from "../components/RegisterRadio.svelte";
  
     let step = 1;
 </script>
@@ -8,9 +9,20 @@
 <aside class="form-container">
     <RegisterHeader step={step} description="Informações Pessoais"/>
     <form>
-        <RegisterInput label="Nome"/>
-        <RegisterInput label="Sobrenome"/>
-        <RegisterInput label="E-mail"/>
+        <div class="grid">
+            <RegisterInput label="Nome"/>
+            <RegisterInput label="Sobrenome"/>
+            <RegisterInput label="E-mail"/>
+            <RegisterInput label="Telefone"/>
+        </div>
+        <div class="section-2">
+            <h1>Gênero</h1>
+            <div class="buttons">
+                <RegisterRadio name="sex" label="Masculino"/>
+                <RegisterRadio name="sex" label="Femenino"/>
+                <RegisterRadio checked={true} name="sex" label="Prefiro não divulgar"/>
+            </div>
+        </div>
     </form>
 </aside>
 
@@ -22,12 +34,32 @@
 }
 
 form{
+    width: 100%;
+    padding: 1rem;
+}
+
+.grid{
     display: grid;
     grid-template-columns: 1fr 1fr; 
     grid-template-rows: auto;
     gap: 1rem;
     width: 100%;
     justify-items: stretch;
-    padding: 1rem;
+}
+
+.section-2{
+    display: flex;
+    flex-direction: column;
+    margin-top: 1rem;
+}
+
+.section-2 h1{
+    font-size: 1rem;
+    margin-bottom: 1rem;
+}
+
+.section-2 .buttons{
+    display: flex;
+    gap: 1rem;
 }
 </style>
--- foran/src/components/RegisterRadio.svelte
+++ foran/src/components/RegisterRadio.svelte
@@ -1,20 +1,66 @@
-<script>
-    let description="pussy";
+<script lang="typescript">
+    import {onMount} from "svelte";
+
+    export let label: string;
+    export let name: string;
+    export let checked: boolean = false;
+    let button: HTMLInputElement;
+
+    onMount(() => {
+        if(checked) button.checked = true;
+    });
+
 </script>
 
-<div>
-    <label for="" class="lab"><input type="radio" class="button"/>{description}</label>
+<div class="container">
+    <div class="outer">
+      <div class="inner"></div>
+    </div>
+    <input id="input" bind:this={button} name={name} type="radio" class="button" hidden/>
+    <label for="" class="lab">{label}</label>
 </div>
 
 <style>
-    .lab{
-        color: var(--color-3);
-        font-family: "Roboto regular";
-        font-size: 1rem;
-        padding: 0.5rem 1rem;
+    .container{
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+        width: fit-content;  
     }
 
-    .button{
-        padding: 2rem;
+    .outer{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 1rem;
+        height: 1rem;
+        border: 1px solid var(--color-11);
+        border-radius: 50%;
+        transition: border-color 0.5s ease;
+    }
+
+    .inner{
+        width: 0.5rem;
+        height: 0.5rem;
+        border-radius: 50%;
+        transition: background-color 0.5s ease;
+    }
+
+    .outer:hover{
+        border-color: var(--color-2);
+    }
+
+    .outer:hover > .inner{
+        background-color: var(--color-2);
+    }
+
+    .lab{
+        color: var(--color-3);
+        font-family: "Roboto";
+        font-size: 0.9rem;
+    }
+
+    input{
+        display: none;
     }
 </style>
