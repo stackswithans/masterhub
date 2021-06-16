8dc5b82f4d15abac8876718b02b87bcc091a0726
stackswithans <stexor12@gmail.com>
2021-06-16 13:09:22 +0100

    Fix deploy bug

--- tilbage/tilbage/asgi.py
+++ tilbage/tilbage/asgi.py
@@ -9,18 +9,22 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 import django
+
+django.setup()
+
 from dotenv import load_dotenv
 from channels.http import AsgiHandler
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter, ChannelNameRouter
 import call.routing
+from call.consumers import CallConsumer
 
 load_dotenv()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tilbage.settings")
-django.setup()
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
         "websocket": URLRouter(call.routing.websocket_urlpatterns),
+        "channel": ChannelNameRouter({"call": CallConsumer.as_asgi()}),
     }
 )
